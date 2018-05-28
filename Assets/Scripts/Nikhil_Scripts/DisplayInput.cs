09a15ffce79de74f0677da66a7092005a6c6eec3
Nikhil Sharma <nik41507@gmail.com>
2018-05-28 13:45:06 -0700

    Switching Scenes
    The main menu can open the first scene, and the first scene can return to the main menu.
    
    The test scene displays how many touches are on the scene

---
+++ Assets/Scripts/Nikhil_Scripts/DisplayInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInput : MonoBehaviour {
+
+    //I made this script to see the inputs from touching and stuff 
+
+    //Touches can be tracked with fingerID
+
+    //tap Count?
+
+    public Text touchCount;
+    public Text touchPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        touchCount.text = "Touch Count : " + Input.touchCount;
+        
+        if(Input.touchCount > 0)
+        {
+            Touch myTouch = Input.GetTouch(0);
+            touchPosition.text = "Touch 1 Positon : " +myTouch.position;
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/Nikhil_Scripts/mainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//This script will be used for changing the scene. So far it is just used by the main menu. There will be a different script for in-game UI
+
+//This script will be expanded upon later with the options menu and level select later. 
+
+public class mainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //goes to the next level. Since this runs of the main menu with a build index of 0 this goes to the 1st level; 
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
