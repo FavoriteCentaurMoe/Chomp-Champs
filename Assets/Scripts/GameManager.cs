a99a0556921a26beca79a72811be2bad582249b2
Zaphero1 <Brgibson@uci.edu>
2018-06-01 17:58:27 -0700

    Playing Sounds
    Picks a random tap sound out of 6 and plays it.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,18 @@ public class GameManager : MonoBehaviour {
     private PlayerController p1;
     private PlayerController p2;
 
+    public AudioClip[] TapSounds;
+    public int NOfSounds;
+
+    public AudioSource AS;
+    System.Random R = new System.Random();
+
+    void playTap(int SoundNumber)
+    {
+        AS.clip = TapSounds[SoundNumber];
+        AS.Play();
+    }
+
     void Start() {
         // If we are starting in the test scene, get the PlayerControllers.
         if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 1) {
@@ -14,29 +27,34 @@ public class GameManager : MonoBehaviour {
             p1 = players[0].GetComponent<PlayerController>();
             p2 = players[1].GetComponent<PlayerController>();
         }
+        AS.GetComponent<AudioSource>();
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
         // Process Touch Inputs.
         for (int i = 0; i < Input.touchCount; ++i) {
             if (Input.GetTouch(i).phase == TouchPhase.Began) {
                 Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
                 RaycastHit hit;
+                
 
-                if (Physics.Raycast(ray, out hit)) {
-                    //Debug.Log("You hit a thing! Here's what it was: " + hit.transform.name);
-                    //Debug.Log("Here is where you hit the thing: " + hit.point);
+                playTap(R.Next(0, NOfSounds));
+                
+            
+            if (Physics.Raycast(ray, out hit)) {
+                //Debug.Log("You hit a thing! Here's what it was: " + hit.transform.name);
+                //Debug.Log("Here is where you hit the thing: " + hit.point);
 
-                    Vector3 lookPoint = new Vector3(hit.point.x, 0, hit.point.z);
-                    if (hit.point.z < 0) {
-                        p1.processTouch(lookPoint);
-                    }
-                    else if (hit.point.z > 0) {
-                        p2.processTouch(lookPoint);
-                    }
-                }                    
+                Vector3 lookPoint = new Vector3(hit.point.x, 0, hit.point.z);
+                if (hit.point.z < 0) {
+                    p1.processTouch(lookPoint);
+                }
+                else if (hit.point.z > 0) {
+                    p2.processTouch(lookPoint);
+                }
             }
         }
-	}
+    }
+    }
 }
